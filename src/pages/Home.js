58608701f658bf4cd7dd3714b1a981e9db12c215
hellomac87 <hellomac87@gmail.com>
2018-09-07 17:45:02 +0900

    javascript 사용하여 링크

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({history}) => {
     return (
         <div>
-         <h2>Home</h2>   
+         <h2>Home</h2>
+         <button onClick={()=> {
+             history.push('./about/javascript')
+         }}>
+         Use javascript link
+         </button>
         </div>
     );
 };
